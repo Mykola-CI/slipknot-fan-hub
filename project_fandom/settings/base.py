27e56bcf4abb6da90434a85475fe7de39fb58907
Mykola Kuievda <dankraig23@gmail.com>
2024-04-25 16:13:33 +0100

    Check styles serviced correctly in deployment

--- project_fandom/settings/base.py
+++ project_fandom/settings/base.py
@@ -61,17 +61,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "project_fandom.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
     'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
 }
--- user_profile/apps.py
+++ user_profile/apps.py
@@ -2,8 +2,7 @@ from django.apps import AppConfig
 
 
 class UserProfileConfig(AppConfig):
-    # for large numbers of users:
-    default_auto_field = 'django.db.models.BigAutoField'
+
     # name of Django application
     name = 'user_profile'
 
