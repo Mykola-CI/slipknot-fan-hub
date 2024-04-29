5c093954d2c0cb9af510aa2d907d9ad4db9b0382
Mykola Kuievda <dankraig23@gmail.com>
2024-04-29 15:50:22 +0100

    Apply initial styles to allauth templates

--- core/views.py
+++ core/views.py
@@ -1,10 +1,11 @@
 from django.views.generic.base import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+# from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class HomeView(TemplateView):
     template_name = 'core/home.html'
 
+    # Making available the user object to the template
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['user'] = self.request.user
